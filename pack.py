bf9bccf6b7e4cadbbe64e9bd53b2f88a47cf866c
omid salami <omid514@proton.me>
2024-08-04 18:33:53 +0330

    put_point function now have a protecter from out side points that given

--- pack.py
+++ pack.py
@@ -1,15 +1,18 @@
 # In the name of God
 
 
-width, height, neviseh_safheh = 100, 40, chr(9608)
+width, height, target_char = 100, 40, chr(9608)
 
-line = neviseh_safheh * width + "\n"
-body = neviseh_safheh + " " * (width - 2) + neviseh_safheh + "\n"
+line = target_char * width + "\n"
+body = target_char + " " * (width - 2) + target_char + "\n"
 body = body * (height - 2)
 
 frame = line + body + line
 
 
 def put_point(x, y, harf, page):
+    if 0 >= x or x >= width or 0 >= y or y >= height:
+        return page
+    
     tedad_char = ((width + 1) * (y + 1)) + (x + 1)
     return page[:tedad_char] + harf + page[tedad_char+1:]

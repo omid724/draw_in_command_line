09c11a2b65b50cce9a72edb43774b697f34cfded
omid salami <omid514@proton.me>
2024-08-05 10:55:41 +0330

    better find out side point to ignore them and added a function to detect the os type

--- pack.py
+++ pack.py
@@ -1,6 +1,9 @@
 # In the name of God
 
 
+import platform
+
+
 width, height, target_char = 100, 40, chr(9608)
 
 line = target_char * width + "\n"
@@ -11,8 +14,19 @@ frame = line + body + line
 
 
 def put_point(x, y, harf, page):
-    if 0 >= x or x >= width or 0 >= y or y >= height:
+    if 0 > x or x >= width - 1 or 0 > y or y >= height - 1:
         return page
-    
+
     tedad_char = ((width + 1) * (y + 1)) + (x + 1)
-    return page[:tedad_char] + harf + page[tedad_char+1:]
+    return page[:tedad_char] + harf + page[tedad_char + 1 :]
+
+
+def what_is_the_os_type():
+    os_type = platform.system()
+
+    if os_type.lower() == "darwin":
+        # it returns 'mac'
+        return "mac"
+    else:
+        # it returns 'windows' or 'linux'
+        return os_type.lower()

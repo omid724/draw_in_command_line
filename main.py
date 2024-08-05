22a1a1a6a5630233973f4dc7f733e0f2287898c8
omid salami <omid514@proton.me>
2024-08-05 11:26:54 +0330

    update

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 
 
 import os
-from pack import frame, put_point, what_is_the_os_type
+from time import sleep
+from pack import frame, put_point, clear_screen
 
 monitor = frame
 xp, yp = 5, 5
@@ -10,11 +11,6 @@ xp, yp = 5, 5
 for y in range(0, 10):
     for x in range(0, 10):
         monitor = put_point(x + xp, y + yp, chr(9608), monitor)
-
-
-if what_is_the_os_type() == "linux":
-    os.system("clear")
-else:
-    os.system("cls")
-
-print(monitor)
+        clear_screen()
+        print(monitor)
+        sleep(0.2)

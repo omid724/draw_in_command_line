76e602ce08dce16556797c2deb660751d5d34bf9
omid salami <omid514@proton.me>
2024-08-13 17:23:02 +0330

    update

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 # In the name of God
 
 
-import os
 from time import sleep
 from pack import frame, put_point, clear_screen
 
 monitor = frame
-xp, yp = 5, 5
+xp, yp = 40, 5
 
-for y in range(0, 10):
-    for x in range(0, 10):
+for y in range(0, 20):
+    for x in range(0, y):
         monitor = put_point(x + xp, y + yp, chr(9608), monitor)
         clear_screen()
         print(monitor)
-        sleep(0.2)
+        sleep(0.02)
